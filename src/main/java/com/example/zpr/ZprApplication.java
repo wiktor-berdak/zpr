89a93b8b9ef0380b9911efd735cc5e8c7a35fce4
Wiktor Berdak <wiciowiec90@gmail.com>
2022-10-11 16:49:55 +0200

    Initial commit

---
+++ src/main/java/com/example/zpr/ZprApplication.java
@@ -0,0 +1,13 @@
+package com.example.zpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zpr/ZprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
